31473770f0b2287ce9873a612a5540e66c4a84f3
emad20111 <emadsaeed20111@gmail.com>
2025-09-09 21:06:05 +0300

    Create DatabaseService.java

---
+++ ProjectPasswordManager/src/projectpasswordmanager/DatabaseService.java
@@ -0,0 +1,8 @@
+package projectpasswordmanager;
+
+import com.sun.jdi.connect.spi.Connection;
+
+public class DatabaseService {
+    private Connection connection ;
+    private DatabaseService instance ;
+}
