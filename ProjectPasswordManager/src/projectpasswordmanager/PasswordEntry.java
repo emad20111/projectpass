c41fbcf8513a1b829ea79b25c2211f954201482a
emad20111 <emadsaeed20111@gmail.com>
2025-09-09 21:06:20 +0300

    Create PasswordEntry.java

---
+++ ProjectPasswordManager/src/projectpasswordmanager/PasswordEntry.java
@@ -0,0 +1,10 @@
+package projectpasswordmanager;
+
+public class PasswordEntry {
+    private int entryid ;
+    private String serviceName ;
+    private String username ;
+    private String encryptedPassword ;
+    private String url ;
+    private String notes ;
+}
