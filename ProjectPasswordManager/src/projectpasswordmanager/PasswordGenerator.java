578356664026a5ad03c3ea7cdbab46b5bba272e3
emad20111 <emadsaeed20111@gmail.com>
2025-09-09 21:06:23 +0300

    Create PasswordGenerator.java

---
+++ ProjectPasswordManager/src/projectpasswordmanager/PasswordGenerator.java
@@ -0,0 +1,8 @@
+package projectpasswordmanager;
+
+public class PasswordGenerator {
+       private int length ;
+       private boolean includeUppercase;
+       private boolean includeNumbers ;
+       private boolean includeSpecialChars ;
+}
