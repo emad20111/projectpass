f8d769242f7e42d43c2c4d18edfbbee09a0e4f63
emad20111 <emadsaeed20111@gmail.com>
2025-09-21 18:22:34 +0300

    Update ProjectPasswordManager.java

--- ProjectPasswordManager/src/projectpasswordmanager/ProjectPasswordManager.java
+++ ProjectPasswordManager/src/projectpasswordmanager/ProjectPasswordManager.java
@@ -1,9 +1,11 @@
 package projectpasswordmanager;
 
+import Classes.DBconnection;
+
 public class ProjectPasswordManager {
 
     public static void main(String[] args) {
-        
+        DBconnection.getConnection();
     }
     
 }
