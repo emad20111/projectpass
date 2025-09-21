b135157fc35d75a7e0fe834e9e990f8440b1be38
emad20111 <emadsaeed20111@gmail.com>
2025-09-21 18:20:44 +0300

    Create BaseEntity.java

---
+++ ProjectPasswordManager/src/Classes/BaseEntity.java
@@ -0,0 +1,19 @@
+package Classes;
+
+public class BaseEntity {
+     private int id;
+    
+    public BaseEntity() {}
+    
+    public BaseEntity(int id) {
+        this.id = id;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+}
