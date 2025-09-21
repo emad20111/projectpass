2460f4473b30baf1a89eefbd9708317c16617db6
emad20111 <emadsaeed20111@gmail.com>
2025-09-21 18:22:15 +0300

    Delete PasswordGenerator.java

--- ProjectPasswordManager/src/projectpasswordmanager/PasswordGenerator.java
+++
@@ -1,8 +0,0 @@
-package projectpasswordmanager;
-
-public class PasswordGenerator {
-       private int length ;
-       private boolean includeUppercase;
-       private boolean includeNumbers ;
-       private boolean includeSpecialChars ;
-}
